30938788cdf2406feb3078ecec8585f0f1e663a7
ignattru <ignatusha.evgeny@gmail.com>
2022-03-22 14:04:58 +1000

    Start to get object

--- src/hibernate_test/entity/Test3.java
+++ src/hibernate_test/entity/Test3.java
@@ -1,4 +1,25 @@
 package hibernate_test.entity;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            session.getTransaction().commit();
+        }
+        finally {
+            factory.close();
+
+        }
+    }
 }
